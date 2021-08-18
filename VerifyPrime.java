47daab9067bc1ba7fc2daf4e56e102850089b3b6
HarshShiyani <56556849+HarshShiyani@users.noreply.github.com>
2021-08-18 11:44:13 +0530

    Create VerifyPrime.java

---
+++ VerifyPrime.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int isPrime(int A) {
+        
+        if(A < 2) return 0;
+
+        for(int i=2; i<=Math.sqrt(A); i++){
+            if(A % i == 0)
+                return 0;
+        }
+        return 1;
+    }
+}
